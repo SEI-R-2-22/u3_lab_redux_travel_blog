a51a8b7c2e4ac897aa61445392f71944cd2c3aa9
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-06 08:16:42 -0600

    updates

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+import PostReducer from 
+
+
 const store = createStore(
   combineReducers({
-    departmentState: DepartmentReducer
+    postsState: PostReducer
   }),
   composeWithDevTools(applyMiddleware(thunk))
 )
